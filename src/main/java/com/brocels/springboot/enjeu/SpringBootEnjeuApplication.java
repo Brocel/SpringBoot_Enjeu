1d8d45af78b316cd8f4a571450139f04dbc3ed7c
Brocel <geenens.prof@gmail.com>
2021-03-16 06:58:40 -0300

    first commit Enjeu springboot web version

---
+++ src/main/java/com/brocels/springboot/enjeu/SpringBootEnjeuApplication.java
@@ -0,0 +1,13 @@
+package com.brocels.springboot.enjeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEnjeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEnjeuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brocels/springboot/enjeu/SpringBootEnjeuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brocels.springboot.enjeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEnjeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
