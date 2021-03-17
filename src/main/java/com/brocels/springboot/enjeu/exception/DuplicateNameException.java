8f434a569171f371c7077d7ead719695b333b7ff
Brocel <geenens.prof@gmail.com>
2021-03-17 09:29:11 -0300

    Edit custom errors

--- src/main/java/com/brocels/springboot/enjeu/exception/DuplicateNameException.java
+++ src/main/java/com/brocels/springboot/enjeu/exception/DuplicateNameException.java
@@ -2,4 +2,10 @@ package com.brocels.springboot.enjeu.exception;
 
 public class DuplicateNameException extends Exception {
 
+	private String errorMessage = "This player name already exists in players list";
+
+	@Override
+	public String getMessage() {
+		return errorMessage;
+	}
 }
---
+++ src/main/java/com/brocels/springboot/enjeu/repository/PlayerRepository.java
@@ -0,0 +1,14 @@
+package com.brocels.springboot.enjeu.repository;
+
+public class PlayerRepository {
+
+	private static int maximumPlayers = 10;
+	
+	
+	
+	
+	public static int getMaximumPlayers() {
+		return maximumPlayers;
+	}
+
+}
