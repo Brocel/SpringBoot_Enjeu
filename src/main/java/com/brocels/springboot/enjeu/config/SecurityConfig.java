bf524991bf76d9a2bb74c9e7a7ff23611668a1d1
Brocel <geenens.prof@gmail.com>
2021-04-14 11:25:18 -0300

    Start tuto pring security config : rename securityConfig file, comment previous config

---
+++ src/main/java/com/brocels/springboot/enjeu/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.brocels.springboot.enjeu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//			.authorizeRequests()
+//				.antMatchers("/", "/home").permitAll()
+//				.antMatchers("/css/**", "/**").permitAll()
+//				.anyRequest().authenticated()
+//				.and()
+//			.formLogin()
+//				.loginPage("/login")
+//				.usernameParameter("name")
+//				.permitAll()
+//				.and()
+//			.logout()
+//				.permitAll();
+//	}
+
+	
+}
